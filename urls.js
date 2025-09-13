18f7502f2b48b85f0902105232ff72deb0470d1e
Counterfactualtheory-gmailcom <frank.harvey@dal.ca>
2025-09-13 09:30:46 -0300

    Add files via upload
    Initial copy of backend files for GPT-5 version

---
+++ urls.js
@@ -0,0 +1,23 @@
+// urls.js
+// Centralized list of approved Dalhousie URLs
+
+const dalUrls = {
+  general: [
+    "https://www.dal.ca",
+    "https://www.ukings.ca"
+  ],
+  studentServices: [
+    "https://www.dal.ca/campus_life/academic-support.html",
+    "https://www.dal.ca/campus_life/health-and-wellness.html",
+    "https://www.dal.ca/campus_life/residence.html"
+  ],
+  research: [
+    "https://www.dal.ca/research",
+    "https://www.nserc-crsng.gc.ca",
+    "https://www.sshrc-crsh.gc.ca",
+    "https://cihr-irsc.gc.ca"
+  ]
+  // Add more categories as needed
+};
+
+module.exports = dalUrls;
